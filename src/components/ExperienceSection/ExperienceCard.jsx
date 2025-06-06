c700ac3eb78cd52dcf94b7261d39c213ef6b392d
Dylan10123 <dbds.101203@gmail.com>
2025-06-06 13:17:48 +0200

    feat: Sección de experiencia responsive

--- src/components/ExperienceSection/ExperienceCard.jsx
+++ src/components/ExperienceSection/ExperienceCard.jsx
@@ -6,22 +6,28 @@ export default function ExperienceCard({
   tecnologias = [],
 }) {
   return (
-    <article
-      className={`bg-zinc-900/70 p-4 rounded-2xl flex flex-col gap-6 shadow-lg`}
-    >
-      <header className="flex flex-col gap-2">
-        <div className="bg-zinc-700/50 w-full h-fit rounded-2xl flex items-center justify-center p-4 mb-4">
-          {logo}
-        </div>
+    <article className="grid grid-cols-4 lg:grid-cols-8 gap-y-8 gap-x-4 font-inter bg-zinc-900/70 p-4 rounded-2xl shadow-lg lg:bg-transparent lg:shadow-none">
+      <div className="col-span-full lg:col-start-6 lg:col-end-8 lg:row-span-5 bg-zinc-700/50 lg:shadow-[-18px_18px_17px_-5px_rgba(0,_0,_0,_0.3)] w-full h-fit lg:h-full rounded-2xl flex items-center justify-center p-4 mb-4">
+        {typeof logo === "string" ? (
+          <img src={logo} alt="" className="h-2/5" />
+        ) : (
+          logo
+        )}
+      </div>
 
-        <h3 className="text-2xl font-bold ">{puesto.toUpperCase()}</h3>
-        <span className="text-violet-400 text-lg text-balance">{empresa}</span>
-      </header>
+      <h3 className="col-span-full lg:col-start-2 lg:col-end-5 lg:row-start-1 text-2xl lg:text-4xl font-bold flex flex-col gap-2">
+        {puesto.toUpperCase()}
+        <span className="text-violet-400 text-lg lg:text-2xl text-balance">
+          {empresa}
+        </span>
+      </h3>
 
-      <div>{children}</div>
+      <div className="col-span-full lg:col-start-2 lg:col-end-5 lg:row-start-3">
+        {children}
+      </div>
 
       {tecnologias.length > 0 && (
-        <div className="flex flex-wrap gap-1">
+        <div className="col-span-full lg:col-start-2 lg:col-end-5 lg:row-start-4 flex flex-wrap gap-1">
           {tecnologias.map((tech, i) => (
             <span
               key={i}
--- src/components/ExperienceSection/ExperienceSection.jsx
+++ src/components/ExperienceSection/ExperienceSection.jsx
@@ -1,9 +1,29 @@
-import { PixelComLogo } from "../../assets/Icons";
+import { PixelComLogoMobile } from "../../assets/Icons";
+import PixelComLogo from "/images/PixelCom-logo_Large.webp";
 import ExperienceCard from "./ExperienceCard";
 import SectionTitle from "../SectionTitle";
 import TextBox from "../TextBox";
+import { useState, useEffect } from "react";
+
+const useIsMobile = (breakpoint = 768) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth < breakpoint);
+    };
+
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, [breakpoint]);
+
+  return isMobile;
+};
 
 export default function ExperienceSection() {
+  const isMobile = useIsMobile();
+
   return (
     <section
       id="experiencia"
@@ -12,33 +32,31 @@ export default function ExperienceSection() {
     >
       <div className="mb-24">
         <SectionTitle text1="¿Qué" highlight="EXPERIENCIA" text2="Tengo?" />
-      </div>
-      <article className="flex flex-col gap-4 font-inter">
-        <ExperienceCard
-          puesto={"Full Stack Developer"}
-          empresa={"PixelCom"}
-          logo={<PixelComLogo />}
-          tecnologias={[
-            "React",
-            "TypeScript",
-            "Storybook",
-            "AWS",
-            "Node.js",
-            "Express",
-            "DynamoDB",
-            "Terraform",
-            "Kubernetes",
-            "WebSockets",
-          ]}
-        >
-          <TextBox>
-            Periodo de formación profesional dual en el que he trabajado con
-            metodologías ágiles (CI/CD) y he participado en el desarrollo de
-            soluciones tanto en el frontend como en el backend, utilizando
-            tecnologías modernas.
-          </TextBox>
-        </ExperienceCard>
-      </article>
+      </div>{" "}
+      <ExperienceCard
+        puesto={"Full Stack Developer"}
+        empresa={"PixelCom"}
+        logo={isMobile ? <PixelComLogoMobile /> : PixelComLogo}
+        tecnologias={[
+          "React",
+          "TypeScript",
+          "Storybook",
+          "AWS",
+          "Node.js",
+          "Express",
+          "DynamoDB",
+          "Terraform",
+          "Kubernetes",
+          "WebSockets",
+        ]}
+      >
+        <TextBox>
+          Periodo de formación profesional dual en el que he trabajado con
+          metodologías ágiles (CI/CD) y he participado en el desarrollo de
+          soluciones tanto en el frontend como en el backend, utilizando
+          tecnologías modernas.
+        </TextBox>
+      </ExperienceCard>
     </section>
   );
 }
